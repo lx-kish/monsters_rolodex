92cc4775aec61ec2746db7bbcebdf903a7809a9b
lx kish <urban.ktulhu@gmail.com>
2020-05-28 12:01:56 +1200

    Updates

---
+++ precache-manifest.957e13e3d6e305dee2fcea6f95c7a3df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ee1583efe6fec7858bae4028d79554",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "514aa4872c0759dde29f",
+    "url": "/monsters_rolodex/static/css/main.26dbcbe0.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters_rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters_rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514aa4872c0759dde29f",
+    "url": "/monsters_rolodex/static/js/main.67e8283c.chunk.js"
+  },
+  {
+    "revision": "e07913cc312f4dec435b",
+    "url": "/monsters_rolodex/static/js/runtime-main.457439c0.js"
+  }
+]);
